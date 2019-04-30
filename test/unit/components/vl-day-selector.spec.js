5bdfd3788650a7b8258dc7d3808f41f9266dff6e
Valdo Ghafoor <valdoghafoor@gmail.com>
2019-04-30 16:21:19 +0200

    Add unit tests

--- test/unit/components/vl-day-selector.spec.js
+++ test/unit/components/vl-day-selector.spec.js
@@ -8,7 +8,8 @@ describe('vl-day-selector', () => {
   function mountComponent (config = {}) {
     wrapper = shallowMount(VLSingleDateSelector, {
       propsData: {
-        selectedDate: config.selectedDate
+        selectedDate: config.selectedDate,
+        disabledDates: config.disabledDates,
       }
     })
     return wrapper
@@ -18,25 +19,80 @@ describe('vl-day-selector', () => {
     mountComponent()
     expect(wrapper.find({ ref: 'calendar' }).exists()).to.be.true
   })
-  
+
   it('emits appropriate events', () => {
     mountComponent()
-    
+
     wrapper.find({ ref: 'calendar' }).vm.$emit('input', '2018-02-05')
     wrapper.find({ ref: 'calendar' }).vm.$emit('input', '2018-03-05')
     wrapper.find({ ref: 'calendar' }).vm.$emit('input', '2018-01-05')
     wrapper.find({ ref: 'calendar' }).vm.$emit('input', '2018-05-05')
-    
+
     expect(wrapper.emitted('input')).to.deep.equal([['2018-02-05'], ['2018-03-05'], ['2018-01-05'], ['2018-05-05']])
     expect(wrapper.emitted().focus).to.have.lengthOf(4)
   })
 
   it('selected date is marked', () => {
     mountComponent({ selectedDate: '2018-02-15' })
-    
+
     const isSelected = wrapper.find({ ref: 'calendar' }).props().isSelected
     expect(isSelected('2018-02-15')).to.be.true
     expect(isSelected('2018-02-14')).to.be.false
     expect(isSelected('2018-02-16')).to.be.false
+  })
+
+  it('disable dates when giving an array of disabled dates', () => {
+    mountComponent({ disabledDates: ['2018-02-15', '2018-02-19'] })
+
+    const isDisabled = wrapper.find({ ref: 'calendar' }).props().isDisabled
+
+
+    expect(isDisabled('2018-02-15')).to.be.true
+    expect(isDisabled('2018-02-19')).to.be.true
+    expect(isDisabled('2019-01-29')).to.be.false
+    expect(isDisabled('2020-01-26')).to.be.false
+  })
+
+  it('disable dates when giving a full object of disabled dates', () => {
+    mountComponent({ disabledDates: {
+      from: '2018-02-15',
+      to: '2018-02-17'
+    } })
+
+    const isDisabled = wrapper.find({ ref: 'calendar' }).props().isDisabled
+    expect(isDisabled('2018-02-15')).to.be.true
+    expect(isDisabled('2018-02-16')).to.be.true
+    expect(isDisabled('2018-02-17')).to.be.true
+    expect(isDisabled('2018-02-18')).to.be.false
+    expect(isDisabled('2018-02-19')).to.be.false
+  })
+
+
+  it('disable dates when giving a partial object of disabled dates', () => {
+    mountComponent({ disabledDates: {
+      from: '2018-02-15',
+    } })
+
+    const isDisabled = wrapper.find({ ref: 'calendar' }).props().isDisabled
+    expect(isDisabled('2018-02-14')).to.be.false
+    expect(isDisabled('2018-02-15')).to.be.true
+    expect(isDisabled('2018-02-16')).to.be.true
+    expect(isDisabled('2018-02-17')).to.be.true
+    expect(isDisabled('2025-02-17')).to.be.true
+    expect(isDisabled('2040-02-17')).to.be.true
+  })
+
+  it('disable dates when giving a different partial object of disabled dates', () => {
+    mountComponent({ disabledDates: {
+      to: '2018-02-15',
+    } })
+
+    const isDisabled = wrapper.find({ ref: 'calendar' }).props().isDisabled
+    expect(isDisabled('2018-02-16')).to.be.false
+    expect(isDisabled('2018-02-15')).to.be.true
+    expect(isDisabled('2018-02-14')).to.be.true
+    expect(isDisabled('2018-02-13')).to.be.true
+    expect(isDisabled('2010-02-13')).to.be.true
+    expect(isDisabled('1990-02-13')).to.be.true
   })
 })
